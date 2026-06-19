42ab2700243e0050465b38b09198064795c96469
Hub-Dashboard-report <26nak.testdev@gmail.com>
2026-06-19 13:02:20 +0700

    chore: add firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAz_o1d-8zk69Zahww0QzeeYAQLv6K2cLU",
+  authDomain: "vehicle-cost-sla.firebaseapp.com",
+  projectId: "vehicle-cost-sla",
+  storageBucket: "vehicle-cost-sla.firebasestorage.app",
+  messagingSenderId: "1013636667495",
+  appId: "1:1013636667495:web:9fb3e147674a2069d11cc2"
 };
